a75f360b5aeb7b24225f9612beac8de193c46cd9
Matt Wilkinson <matt@mattwilkinson.dev>
2022-09-30 19:02:24 -0400

    begin creating routes
    Signed-off-by: Matt Wilkinson <matt@mattwilkinson.dev>

--- actors/realworld-api/src/lib.rs
+++ actors/realworld-api/src/lib.rs
@@ -1,5 +1,6 @@
 use wasmbus_rpc::actor::prelude::*;
 use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+use wasmcloud_interface_logging::{debug, error, info, warn};
 
 #[derive(Debug, Default, Actor, HealthResponder)]
 #[services(Actor, HttpServer)]
@@ -8,7 +9,6 @@ struct RealworldApiActor {}
 /// Implementation of HttpServer trait methods
 #[async_trait]
 impl HttpServer for RealworldApiActor {
-
     /// Returns a greeting, "Hello World", in the response body.
     /// If the request contains a query parameter 'name=NAME', the
     /// response is changed to "Hello NAME"
@@ -17,15 +17,14 @@ impl HttpServer for RealworldApiActor {
         _ctx: &Context,
         req: &HttpRequest,
     ) -> std::result::Result<HttpResponse, RpcError> {
-        let text = form_urlencoded::parse(req.query_string.as_bytes())
-            .find(|(n, _)| n == "name")
-            .map(|(_, v)| v.to_string())
-            .unwrap_or_else(|| "World".to_string());
+        debug!("API request: {:?}", req);
 
-        Ok(HttpResponse {
-            body: format!("Hello {}", text).as_bytes().to_vec(),
-            ..Default::default()
-        })
+        let path = &req.path[1..req.path.len()];
+        let segments: Vec<&str> = path.trim_end_matches('/').split('/').collect();
+        debug!("Segments: {:?}", segments);
+        match (req.method.as_ref(), segments.as_slice()) {
+            ("GET", ["user"]) => Ok(HttpResponse::not_found()),
+            (_, _) => Ok(HttpResponse::not_found()),
+        }
     }
 }
-
